d124b1ff2262d9e97df31cfb30f6bf93aa9de51c
Marcy Hundschell Dela Cruz <90773594+marcy1214@users.noreply.github.com>
2022-05-15 17:54:35 +0200

    node exercise

---
+++ modules.js
@@ -0,0 +1,7 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-flavor')
+require('./mind-grenade')
+// sayHi('marcy')
+// sayHi('noah')
+
---
+++ names.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const marcy = 'marcy'
+const noah = 'noah'
+
+module.exports = { marcy, noah }
